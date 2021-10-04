329d3ea8b5a3dd7264e9a1c4c2196d8ec99a29d4
james <jamesodwuor4@gmail.com>
2021-10-04 12:27:54 +0300

    ft-set sidebar opening function

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import Web from "./mobile";
+import Mobile from "./mobile";
 
 
 function Header(){
-    return <div>This is Header</div>;
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="header">
+            <div className="logo">James Aduke</div>
+            <div className="menu">
+                <div className="WebMenu"><Web /></div>
+                <div className="mobileMenu">
+                    <div onClick={() => {
+                        setIsOpen(!isOpen);
+                    }}>
+                        <i class="fi fi-rr-menu-burger"/>
+                    </div>
+                    {isOpen && <Mobile isOpen = {isOpen} setIsOpen={setIsOpen}/>}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
---
+++ src/components/header/web/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Web (){
+    return <div>WEB</div>
+}
+
+export default Web;
