86663a545eeed21e768701b135b990371c16c58a
james <jamesodwuor4@gmail.com>
2021-10-15 18:06:48 +0300

    add about markup

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import './about.css';
 
 function About (){
-    return <div>This is About</div>;
+    return (
+        <div className="about">
+            <div className="about-top">
+                <div className="about-info">
+                    Hello there, I am
+                    <br/><span className="info-name">James Aduke</span>
+                    <br/>I love experimenting with software
+                </div>
+                <div className="about-photo">
+                    <img src={require('../../../assets/coding.png').default} className="illustration" alt=""/>
+                </div>
+            </div>
+            <div className="about-bottom">
+
+            </div>
+        </div>
+    );
+
 }
 
 export default About;
