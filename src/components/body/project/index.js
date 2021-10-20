1e93b1506e88c6becd70f4517814b09e579b4d57
james <jamesodwuor4@gmail.com>
2021-10-20 19:49:07 +0300

    add seperator common components

--- src/components/body/project/index.js
+++ src/components/body/project/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./projects.css"
 import {ProjectData} from "../../../data/projects";
 import ProjectCard from './project-card';
+import Seperator from "../../../common/seperator";
 
 function Projects (){
     const data = ProjectData;
     return(
         <div className="projects">
+            <Seperator />
             <label className="section-title">Projects</label>
             <div>
                 {data.map((project) => {
