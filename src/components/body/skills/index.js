0837247711981b6d5ad92a5080023c52b3d3b8f5
james <jamesodwuor4@gmail.com>
2021-10-27 13:17:43 +0300

    add skills section component

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import './skills.css';
+import Seperator from "../../../common/seperator";
+import {SkillsData} from "../../../data/skills";
+import SkillCard from "./skill-card";
 
 function Skills (){
-    return <div>This is Skills</div>;
+    const data = SkillsData;
+    return (
+        <div className="skills">
+            <Seperator />
+            <label className="section-title">Skills</label>
+            <div className="skills-container">
+                {data.map((item) =>{
+                    return (
+                        <div className="skills-section">
+                            <label className="skills-section-title">{item.type}</label>
+                            <div className="skills-list">
+                                {/* eslint-disable-next-line array-callback-return */}
+                                {item.list.map((skill) =>{
+                                    return <SkillCard skill={skill} />;
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
---
+++ src/data/skills.js
@@ -0,0 +1,40 @@
+export const SkillsData = [
+    {
+        type: 'Frontend',
+        list: [
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"/>,
+            },
+            {
+                name: "JavaScript",
+                icon: <i class="devicon-javascript-plain colored"/>,
+            },
+            {
+                name: "CSS",
+                icon: <i class="devicon-css3-plain colored"/>,
+            },
+
+        ],
+    },
+
+    {
+        type: 'Backend',
+        list: [
+            {
+                name: "NodeJS",
+                icon: <i class="devicon-nodejs-plain colored"/>,
+            },
+            {
+                name: "Python",
+                icon: <i class="devicon-python-plain colored"/>,
+            },
+            {
+                name: "Ruby",
+                icon: <i class="devicon-ruby-plain colored"/>,
+            },
+
+        ],
+    },
+
+];
